cbba320ecedca0963899dfc11efb6f344dff5b7a
wwdfdo <wwdfdo@gmail.com>
2022-09-12 14:01:21 +0530

    added images

--- src/components/heatmap.js
+++ src/components/heatmap.js
@@ -1,68 +1,184 @@
 import React from "react";
+import nft1 from "../images/nft1.png";
+import nft2 from "../images/nft2.png";
+import nft3 from "../images/nft3.png";
+import nft4 from "../images/nft4.png";
+import nft5 from "../images/nft5.png";
+import nft6 from "../images/nft6.png";
+import nft7 from "../images/nft7.png";
+import nft8 from "../images/nft8.png";
+import nft9 from "../images/nft9.png";
+import nft10 from "../images/nft10.png";
+import nft11 from "../images/nft11.png";
+import nft12 from "../images/nft12.png";
+import nft13 from "../images/nft13.png";
+import nft14 from "../images/nft14.png";
+import nft15 from "../images/nft15.png";
+import nft16 from "../images/nft16.png";
+import nft17 from "../images/nft17.png";
+import nft18 from "../images/nft18.png";
+import nft19 from "../images/nft19.png";
+import nft30 from "../images/nft30.png";
+import nft31 from "../images/nft31.png";
+import nft32 from "../images/nft32.png";
+import nft33 from "../images/nft33.png";
+import nft34 from "../images/nft34.png";
+import nft35 from "../images/nft35.png";
+import nft37 from "../images/nft37.png";
+import nft38 from "../images/nft38.png";
+import nft39 from "../images/nft39.png";
+import nft40 from "../images/nft40.png";
+import nft41 from "../images/nft41.png";
+import nft42 from "../images/nft42.png";
+import nft50 from "../images/nft50.png";
+import nft51 from "../images/nft51.png";
+import nft52 from "../images/nft52.png";
+import nft53 from "../images/nft53.png";
+import nft54 from "../images/nft54.png";
+import nft55 from "../images/nft55.png";
+import nft56 from "../images/nft56.png";
+import nft57 from "../images/nft57.png";
 
 const heatmap = () => {
   return (
-    <div className="grid grid-cols-2 h-[100vh]">
+    <div className="grid grid-cols-2 min-h-[100vh]">
       <div className=" bg-blue-400 grid grid-rows-2">
         <div className=" bg-red-500 grid grid-cols-2">
           <div className=" bg-blue-500 grid grid-rows-2">
-            <div></div>
+            <div className=" bg-black grid grid-cols-2">
+              <div>
+                <img src={nft1} alt="" />
+              </div>
+              <div>
+                <img src={nft2} alt="" />
+              </div>
+            </div>
             <div className="grid grid-cols-2">
-              <div></div>
+              <div>
+                <img src={nft3} alt="" />
+              </div>
               <div className=" bg-red-300 grid grid-cols-2 grid-rows-2">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div className=" bg-blue-800"></div>
+                <div>
+                  <img src={nft4} alt="" />
+                </div>
+                <div>
+                  <img src={nft5} alt="" />
+                </div>
+                <div>
+                  <img src={nft6} alt="" />
+                </div>
+                <div className=" bg-blue-800">
+                  <img src={nft7} alt="" />
+                </div>
               </div>
             </div>
           </div>
           <div className="grid grid-cols-2 grid-rows-2">
-            <div></div>
-            <div className=" bg-blue-500 grid grid-cols-2 grid-rows-2">
-              <div></div>
-              <div className=" bg-blue-800"></div>
-              <div></div>
-              <div></div>
+            <div>
+              <img src={nft8} alt="" />
             </div>
-            <div></div>
-            <div></div>
+            <div className=" bg-blue-500 grid grid-cols-2 grid-rows-2">
+              <div>
+                <img src={nft9} alt="" />
+              </div>
+              <div className=" bg-blue-800">
+                <img src={nft11} alt="" />
+              </div>
+              <div>
+                <img src={nft12} alt="" />
+              </div>
+              <div>
+                <img src={nft10} alt="" />
+              </div>
+            </div>
+            <div>
+              <img src={nft14} alt="" />
+            </div>
+            <div>
+              <img src={nft13} alt="" />
+            </div>
           </div>
         </div>
         <div className=" bg-green-500 grid grid-cols-2">
           <div className=" bg-yellow-500 grid grid-cols-2 grid-rows-2">
-            <div></div>
-            <div></div>
-            <div className=" bg-blue-500 grid grid-cols-2 grid-rows-2">
-              <div></div>
-              <div></div>
-              <div className="bg-yellow-300 grid grid-cols-2 grid-rows-2">
-                <div></div>
-                <div></div>
-                <div className="bg-green-500"></div>
-                <div></div>
-              </div>
-              <div></div>
+            <div>
+              <img src={nft4} alt="" />
             </div>
-            <div></div>
+            <div>
+              <img src={nft5} alt="" />
+            </div>
+            <div className=" bg-blue-500 grid grid-cols-2 grid-rows-2">
+              <div>
+                <img src={nft7} alt="" />
+              </div>
+              <div>
+                <img src={nft3} alt="" />
+              </div>
+              <div className="bg-yellow-300 grid grid-cols-2 grid-rows-2">
+                <div>
+                  <img src={nft8} alt="" />
+                </div>
+                <div>
+                  <img src={nft13} alt="" />
+                </div>
+                <div className="bg-green-500">
+                  <img src={nft15} alt="" />
+                </div>
+                <div>
+                  <img src={nft8} alt="" />
+                </div>
+              </div>
+              <div>
+                <img src={nft4} alt="" />
+              </div>
+            </div>
+            <div>
+              <img src={nft6} alt="" />
+            </div>
           </div>
           <div className=" grid grid-cols-2 grid-rows-2">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <img src={nft4} alt="" />
+            </div>
+            <div>
+              <img src={nft7} alt="" />
+            </div>
+            <div>
+              <img src={nft11} alt="" />
+            </div>
             <div className="bg-blue-800 grid grid-cols-2 grid-rows-2">
-              <div></div>
-              <div></div>
-              <div></div>
+              <div>
+                <img src={nft13} alt="" />
+              </div>
+              <div>
+                <img src={nft15} alt="" />
+              </div>
+              <div>
+                <img src={nft19} alt="" />
+              </div>
               <div className=" bg-yellow-300 grid grid-cols-2 grid-rows-2">
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                  <img src={nft4} alt="" />
+                </div>
+                <div>
+                  <img src={nft11} alt="" />
+                </div>
+                <div>
+                  <img src={nft12} alt="" />
+                </div>
                 <div className=" bg-red-500 grid grid-cols-2 grid-rows-2">
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div className="bg-purple-600"></div>
+                  <div>
+                    <img src={nft15} alt="" />
+                  </div>
+                  <div>
+                    <img src={nft4} alt="" />
+                  </div>
+                  <div>
+                    <img src={nft11} alt="" />
+                  </div>
+                  <div className="bg-purple-600">
+                    <img src={nft18} alt="" />
+                  </div>
                 </div>
               </div>
             </div>
@@ -72,48 +188,113 @@ const heatmap = () => {
       <div className=" bg-yellow-400 grid grid-rows-2">
         <div className="grid grid-cols-2">
           <div className="grid grid-cols-2 grid-rows-2">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <img src={nft30} alt="" />
+            </div>
+            <div>
+              <img src={nft31} alt="" />
+            </div>
+            <div>
+              <img src={nft32} alt="" />
+            </div>
             <div className=" bg-blue-600 grid grid-cols-2 grid-rows-2">
-              <div></div>
-              <div></div>
-              <div></div>
+              <div>
+                <img src={nft33} alt="" />
+              </div>
+              <div>
+                <img src={nft35} alt="" />
+              </div>
+              <div>
+                <img src={nft30} alt="" />
+              </div>
               <div className=" bg-red-600 grid grid-cols-2 grid-rows-2">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div className=" bg-purple-600"></div>
+                <div>
+                  <img src={nft31} alt="" />
+                </div>
+                <div>
+                  <img src={nft32} alt="" />
+                </div>
+                <div>
+                  <img src={nft33} alt="" />
+                </div>
+                <div className=" bg-purple-600">
+                  <img src={nft35} alt="" />
+                </div>
               </div>
             </div>
           </div>
           <div className=" bg-green-500 grid grid-cols-2 grid-rows-2">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <img src={nft37} alt="" />
+            </div>
+            <div>
+              <img src={nft38} alt="" />
+            </div>
+            <div>
+              <img src={nft32} alt="" />
+            </div>
             <div className=" bg-purple-600 grid grid-cols-2 grid-rows-2">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div className=" bg-pink-600"></div>
+              <div>
+                <img src={nft40} alt="" />
+              </div>
+              <div>
+                <img src={nft41} alt="" />
+              </div>
+              <div>
+                <img src={nft42} alt="" />
+              </div>
+              <div className=" bg-pink-600">
+                <img src={nft33} alt="" />
+              </div>
             </div>
           </div>
         </div>
         <div className=" bg-blue-500 grid grid-cols-2">
           <div className="grid grid-cols-3 grid-rows-3">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div className="bg-black grid grid-cols-3 grid-rows-3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div className=" bg-white"></div>
-              <div></div>
+            <div>
+              <img src={nft50} alt="" />
             </div>
-            <div></div>
+            <div>
+              <img src={nft51} alt="" />
+            </div>
+            <div>
+              {" "}
+              <img src={nft52} alt="" />
+            </div>
+            <div>
+              {" "}
+              <img src={nft53} alt="" />
+            </div>
+            <div className="bg-black grid grid-cols-3 grid-rows-3">
+              <div>
+                {" "}
+                <img src={nft55} alt="" />
+              </div>
+              <div>
+                {" "}
+                <img src={nft56} alt="" />
+              </div>
+              <div>
+                {" "}
+                <img src={nft54} alt="" />
+              </div>
+              <div>
+                {" "}
+                <img src={nft50} alt="" />
+              </div>
+              <div className=" bg-white">
+                {" "}
+                <img src={nft51} alt="" />
+              </div>
+              <div>
+                {" "}
+                <img src={nft53} alt="" />
+              </div>
+            </div>
+            <div>
+              {" "}
+              <img src={nft55} alt="" />
+            </div>
           </div>
           <div className=" bg-red-500 grid grid-cols-2 grid-rows-2">
             <div></div>
